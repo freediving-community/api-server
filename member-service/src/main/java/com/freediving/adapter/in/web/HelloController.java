6fe7fc008ec0157ba277f868c45720e6bcce0923
YounggikimA <rladudrl286@naver.com>
2023-12-16 12:45:48 +0900

    init

---
+++ member-service/src/main/java/com/freediving/adapter/in/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.freediving.adapter.in.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+}
