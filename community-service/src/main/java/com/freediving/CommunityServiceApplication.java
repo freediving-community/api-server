1769975b197b1948513b615cdb17e1de44bd72d4
YounggikimA <rladudrl286@naver.com>
2023-12-19 21:51:45 +0900

    refactor: root build.gradle 수정
    멀티모듈 설계 https://youtu.be/rE89ppAmf_Y?si=1ooZw4kzX3jOHaCf

---
+++ community-service/src/main/java/com/freediving/CommunityServiceApplication.java
@@ -0,0 +1,11 @@
+package com.freediving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityServiceApplication.class, args);
+	}
+}
