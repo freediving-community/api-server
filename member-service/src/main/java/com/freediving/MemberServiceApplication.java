c09188a2c5ea2224702e8a2797453be5cc44726e
sasca37 <sasca37@naver.com>
2023-12-24 17:59:09 +0900

    feat: #10 - MemberService 유레카 Client 등록 설정

--- member-service/src/main/java/com/freediving/MemberServiceApplication.java
+++ member-service/src/main/java/com/freediving/MemberServiceApplication.java
@@ -2,10 +2,12 @@ package com.freediving;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MemberServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MemberServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MemberServiceApplication.class, args);
+	}
 }
