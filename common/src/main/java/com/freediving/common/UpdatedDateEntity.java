c33d4e7c4c7ec30ed72e126cb97e2d10926f9828
조준희 <46863381+RedJunHee@users.noreply.github.com>
2023-12-26 13:50:17 +0900

    feat: #17 - Entity Auditing 관리
    Common 모듈 생성
     - Auditing 관리 객체 3가지
       - AuditableEntity ( CreatedDate, UpdatedDate Auditing )
       - CreatedDateEntity ( CreatedDate Auditing )
       - UpdatedDateEntity( UpdatedDate Auditing )
    
    Closes #17

---
+++ common/src/main/java/com/freediving/common/UpdatedDateEntity.java
@@ -0,0 +1,19 @@
+package com.freediving.common;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class UpdatedDateEntity {
+
+	@LastModifiedDate
+	private LocalDateTime updatedDate;
+}
