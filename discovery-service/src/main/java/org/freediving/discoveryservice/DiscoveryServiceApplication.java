9bf8df3f8296b9bfdcc33bc562cbefb0b5499ade
sasca37 <sasca37@naver.com>
2024-01-06 18:11:22 +0900

    style: #30 - DicoveryService 패키지 경로 추가

--- discovery-service/src/main/java/org/freediving/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/org/freediving/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package org.freediving;
+package org.freediving.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
