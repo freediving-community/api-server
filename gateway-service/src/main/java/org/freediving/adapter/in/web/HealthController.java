66b9af72ebc6217fcf34efd3b7211f95bc4c7ffc
조준희 <46863381+RedJunHee@users.noreply.github.com>
2024-01-15 23:28:01 +0900

    feat: Gateway HealthCheck API 적용
    GET http://buddyme/health 요청시 200응답 확인. + 더미 데이터 응답.

--- gateway-service/src/main/java/org/freediving/adapter/in/web/HealthController.java
+++ gateway-service/src/main/java/org/freediving/adapter/in/web/HealthController.java
@@ -2,14 +2,12 @@ package org.freediving.adapter.in.web;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-	@CrossOrigin(origins = "*")
 	@GetMapping("/health")
 	public ResponseEntity<String> healthCheck() {
 
