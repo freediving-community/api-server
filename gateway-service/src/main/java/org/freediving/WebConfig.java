b3632ddd9682f60694adfb0ae37d8a4b4f82be84
조준희 <46863381+RedJunHee@users.noreply.github.com>
2024-01-16 00:43:47 +0900

    feat: Gateway HealthCheck API 적용
    GET http://buddyme/health 요청시 200응답 확인. + 더미 데이터 응답.

--- gateway-service/src/main/java/org/freediving/WebConfig.java
+++ gateway-service/src/main/java/org/freediving/WebConfig.java
@@ -1,12 +1,13 @@
 package org.freediving;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+@EnableWebFlux
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
+public class WebConfig implements WebFluxConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
