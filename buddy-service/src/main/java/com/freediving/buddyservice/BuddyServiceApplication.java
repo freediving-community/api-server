9214f444ec84b655d50bbcd983a8861ca8089ae4
조준희 <46863381+RedJunHee@users.noreply.github.com>
2024-01-16 11:48:50 +0900

    deploy: BuddyService Ci/CD 테스트
    BuddyService Ci/CD 테스트

--- buddy-service/src/main/java/com/freediving/buddyservice/BuddyServiceApplication.java
+++ buddy-service/src/main/java/com/freediving/buddyservice/BuddyServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class BuddyServiceApplication {
+public class BuddyServiceApplication {//push test
+
 	public static void main(String[] args) {
 		SpringApplication.run(BuddyServiceApplication.class, args);
 	}
