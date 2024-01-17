8e0729914e462448540092b649b5b0de1f4c2719
sasca37 <sasca37@naver.com>
2024-01-18 00:30:57 +0900

    docs: #57 - 소셜 로그인 기능 주석 추가

--- member-service/src/main/java/com/freediving/authservice/application/port/in/OauthUseCase.java
+++ member-service/src/main/java/com/freediving/authservice/application/port/in/OauthUseCase.java
@@ -1,13 +1,23 @@
 package com.freediving.authservice.application.port.in;
 
-import com.freediving.authservice.adapter.in.web.UserLoginResponse;
 import com.freediving.authservice.domain.OauthType;
+import com.freediving.authservice.domain.OauthUser;
 import com.freediving.common.config.annotation.UseCase;
+
+/**
+ * @Author         : sasca37
+ * @Date           : 2024/01/18
+ * @Description    : Oauth 정보 제공 및 로그인 기능을 수행하는 UseCase
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * ===========================================================
+ * 2024/01/18        sasca37       최초 생성
+ */
 
 @UseCase
 public interface OauthUseCase {
 
 	String provideOauthType(OauthType oauthType);
 
-	UserLoginResponse login(OauthType oauthType, String code);
+	OauthUser login(OauthType oauthType, String code);
 }
--- member-service/src/main/java/com/freediving/authservice/application/port/out/OauthTemplate.java
+++ member-service/src/main/java/com/freediving/authservice/application/port/out/OauthTemplate.java
@@ -15,12 +15,29 @@ import com.freediving.authservice.domain.OauthUser;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * @Author         : sasca37
+ * @Date           : 2023/12/31
+ * @Description    : 요청 온 소셜 로그인 별 타입 검증, 토큰 및 인증 정보 등의 요청 작업 Template
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * ===========================================================
+ * 2023/12/31        sasca37       최초 생성
+ */
+
 @Component
 @RequiredArgsConstructor
 public class OauthTemplate implements InitializingBean {
 	private final List<OauthFeignPort> feignPortList;
 	private Map<OauthType, OauthFeignPort> feignPortMap;
 
+	/**
+	 * @Author           : sasca37
+	 * @Date             : 2023/12/31
+	 * @Param            :
+	 * @Return           : 로그인 팝업 URL
+	 * @Description      : 요청온 소셜 타입을 검증하고, 팝업 URL을 가져올 수 있는 경우 반환
+	 */
 	public String doRequest(OauthType oauthType) {
 		return Optional.ofNullable(feignPortMap.get(oauthType))
 			.filter(port -> port.canRequest(oauthType))
@@ -28,6 +45,13 @@ public class OauthTemplate implements InitializingBean {
 			.orElseThrow(() -> new NoSuchElementException("No OauthRedirectUrlPort found for " + oauthType));
 	}
 
+	/**
+	 * @Author           : sasca37
+	 * @Date             : 2023/12/31
+	 * @Param            :
+	 * @Return           : OauthUser
+	 * @Description      : 사용자 정보 요청을 위한 토큰 정보를 가져와서 요청 후 유저 정보 반환
+	 */
 	public OauthUser doPostTokenAndGetInfo(OauthType oauthType, String code) {
 		return Optional.ofNullable(feignPortMap.get(oauthType))
 			.filter(port -> port.canRequest(oauthType))
@@ -35,6 +59,13 @@ public class OauthTemplate implements InitializingBean {
 			.orElseThrow(() -> new NoSuchElementException("No OauthFeignPort found for " + oauthType));
 	}
 
+	/**
+	 * @Author           : sasca37
+	 * @Date             : 2023/12/31
+	 * @Param            :
+	 * @Return           : void
+	 * @Description      : 빈 초기화 시점에 Port에 저장되어 있는 구현체 정보를 불러와 요청 온 타입 확인 및 검증
+	 */
 	@Override
 	public void afterPropertiesSet() {
 		feignPortMap = feignPortList.stream()
