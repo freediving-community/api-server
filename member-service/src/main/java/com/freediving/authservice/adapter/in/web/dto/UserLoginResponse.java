3bcbf99288c1cb6cd1554e45f867527248ecf780
sasca37 <sasca37@naver.com>
2024-01-19 00:23:52 +0900

    docs: #59 - 소셜 로그인 Swagger 문서 추가

--- member-service/src/main/java/com/freediving/authservice/adapter/in/web/dto/UserLoginResponse.java
+++ member-service/src/main/java/com/freediving/authservice/adapter/in/web/dto/UserLoginResponse.java
@@ -3,6 +3,7 @@ package com.freediving.authservice.adapter.in.web.dto;
 import com.freediving.authservice.domain.OauthType;
 import com.freediving.authservice.domain.OauthUser;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,15 +20,25 @@ import lombok.Getter;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Schema(description = "유저 로그인 응답 정보")
 public class UserLoginResponse {
+
+	@Schema(description = "소셜 로그인 타입", example = "KAKAO")
 	private OauthType oauthType;
+
+	@Schema(description = "이메일", example = "sasca37@naver.com")
 	private String email;
+
+	@Schema(description = "이메일", example = "https://aws-s3.com")
 	private String profileImgUrl;
 
+	@Schema(description = "액세스 토큰", example = "aaaaaaa.bbbbbbb.ccccccc")
 	private String accessToken;
 
+	@Schema(description = "리프레시 토큰", example = "aaaaaaa.bbbbbbb.ccccccc")
 	private String refreshToken;
 
+	@Schema(description = "최초 가입 여부", example = "true")
 	private Boolean isNewUser;
 
 	public static UserLoginResponse from(OauthUser oauthUser) {
