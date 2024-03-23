4abc8d316e74f289fab2d2c59a70fe1d61c65e09
sasca37 <sasca37@naver.com>
2024-03-24 00:21:57 +0900

    feat: #97 - 샘플 유저 생성 기능 추가

--- member-service/src/main/java/com/freediving/memberservice/adapter/in/web/HelloController.java
+++ member-service/src/main/java/com/freediving/memberservice/adapter/in/web/HelloController.java
@@ -1,16 +1,44 @@
 package com.freediving.memberservice.adapter.in.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.freediving.authservice.util.JwtTokenUtils;
+import com.freediving.memberservice.application.port.in.CreateUserCommand;
+import com.freediving.memberservice.application.port.in.CreateUserUseCase;
+import com.freediving.memberservice.domain.OauthType;
+import com.freediving.memberservice.domain.User;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @Tag(name = "Ping", description = "MemberService Health Check")
+@RequiredArgsConstructor
+@RequestMapping("/sample")
 public class HelloController {
+
+	private final CreateUserUseCase createUserUseCase;
+	@Value("${jwt.key}")
+	private String key;
 
 	@GetMapping("/")
 	public String hello() {
 		return "Hello";
+	}
+
+	@PostMapping("/token")
+	public String generateSampleUser() {
+		CreateUserCommand command = CreateUserCommand.builder()
+			.oauthType(OauthType.KAKAO)
+			.email("sasca37@naver.com")
+			.profileImgUrl("https://img.com")
+			.build();
+		User user = createUserUseCase.createOrGetUser(command);
+		return JwtTokenUtils.generateRefreshToken(String.valueOf(user.userId()),
+			String.valueOf(user.oauthType()), key);
 	}
 }
