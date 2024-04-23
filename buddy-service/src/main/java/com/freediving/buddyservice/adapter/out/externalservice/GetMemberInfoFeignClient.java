53bc083d8283f75581df72cd057d910fa6ae08d4
조준희 <46863381+RedJunHee@users.noreply.github.com>
2024-04-23 18:59:04 +0900

    feat: 버디 이벤트 조회하기 컴포넌트 개발

--- buddy-service/src/main/java/com/freediving/buddyservice/adapter/out/externalservice/GetMemberInfoFeignClient.java
+++ buddy-service/src/main/java/com/freediving/buddyservice/adapter/out/externalservice/GetMemberInfoFeignClient.java
@@ -1,5 +1,7 @@
 package com.freediving.buddyservice.adapter.out.externalservice;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "get/v1/service/users", url = "${feign.external-service.member-service}")
 public interface GetMemberInfoFeignClient {
 	@GetMapping("/v1/service/users")
-	Object getMemberInfo(@RequestParam Integer[] userIds,
+	Object getMemberInfo(@RequestParam List<Long> userIds,
 		@RequestParam Boolean profileImg);
 }
--- buddy-service/src/main/java/com/freediving/buddyservice/adapter/out/externalservice/MemberServiceAdapter.java
+++ buddy-service/src/main/java/com/freediving/buddyservice/adapter/out/externalservice/MemberServiceAdapter.java
@@ -1,16 +1,25 @@
 package com.freediving.buddyservice.adapter.out.externalservice;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.freediving.buddyservice.application.port.out.externalservice.query.MemberStatus;
 import com.freediving.buddyservice.application.port.out.externalservice.query.RequestMemberPort;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
+@RequiredArgsConstructor
 public class MemberServiceAdapter implements RequestMemberPort {
+
+	private final GetMemberInfoFeignClient getMemberInfoFeignClient;
 
 	//TODO 멤버 유효성 요청 구현 필요.
 	@Override
 	public MemberStatus getMemberStatus(Long userId) {
+
+		getMemberInfoFeignClient.getMemberInfo(List.of(userId), false);
 		return MemberStatus.builder().userid(userId).isValid(true).build();
 	}
 }
