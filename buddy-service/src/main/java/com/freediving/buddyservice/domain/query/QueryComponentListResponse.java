4c442eda55c234c6bdb73420f0e7714afb2a59cf
조준희 <46863381+RedJunHee@users.noreply.github.com>
2024-04-23 17:08:41 +0900

    feat: 버디 이벤트 조회하기 컴포넌트 개발

--- buddy-service/src/main/java/com/freediving/buddyservice/domain/query/QueryComponentListResponse.java
+++ buddy-service/src/main/java/com/freediving/buddyservice/domain/query/QueryComponentListResponse.java
@@ -5,20 +5,29 @@ import java.util.List;
 
 import com.freediving.buddyservice.domain.query.component.QueryComponent;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
-* QueryComponent객체를 List형식으로 Response할떄 사용하는 DTO이다.
+ * QueryComponent객체를 List형식으로 Response할떄 사용하는 DTO이다.
  * 기본적으로 페이징을 한다고 가정을 하고 있다.
  *   => totalCount, count 페이지 정보를 지니고 있다.
-*
-* @author 조준희
-* @version 1.0.0
-* 작성일 2024-04-06
-**/
+ *
+ * @author 조준희
+ * @version 1.0.0
+ * 작성일 2024-04-06
+ **/
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class QueryComponentListResponse {
 
 	private List<QueryComponent> components = new ArrayList<>();
 	// 컴포넌트 총 개수.
-	private Integer totalCount; //todo 페이징 부가 정보들은 추후 협의 후에 추가 해야함.
+	private Integer totalCount;
+	private Integer page; // 요청한 카운트의 페이지 넘버
+	private Integer currentCount; // 요청한 카운트
 }
---
+++ buddy-service/src/main/java/com/freediving/buddyservice/domain/query/component/common/ParticipantInfoResponse.java
@@ -0,0 +1,15 @@
+package com.freediving.buddyservice.domain.query.component.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class ParticipantInfoResponse {
+	private Long userId;
+	private String profileUrl;
+}
