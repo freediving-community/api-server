a882289345da123bde56ab3a57f61a070d15b94b
YounggikimA <rladudrl286@naver.com>
2024-04-27 00:44:07 +0900

    feat: #138 - 커뮤니티 XSS 적용 및 API 분리
    - ResponseBody JSON데이터 내 XSS 방지 처리

---
+++ community-service/src/main/java/com/freediving/communityservice/config/HtmlCharacterEscapes.java
@@ -0,0 +1,46 @@
+package com.freediving.communityservice.config;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+
+public class HtmlCharacterEscapes extends CharacterEscapes {
+
+	private final int[] asciiEscapes;
+
+	public HtmlCharacterEscapes() {
+		// 1. XSS 방지 처리할 특수 문자 지정
+		asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+		asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+		asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+		asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+		asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
+		asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
+		asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
+		asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+	}
+
+	@Override
+	public int[] getEscapeCodesForAscii() {
+		return asciiEscapes;
+	}
+
+	@Override
+	public SerializableString getEscapeSequence(int ch) {
+		SerializedString serializedString = null;
+		char charAt = (char)ch;
+
+		if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("\\u");
+			sb.append(String.format("%04x", ch));
+			serializedString = new SerializedString(sb.toString());
+		} else {
+			serializedString = new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+		}
+
+		return serializedString;
+	}
+}
