238c3f19917235c61a179bfe464f85df721c3f1c
YounggikimA <rladudrl286@naver.com>
2024-04-27 16:43:56 +0900

    feat: #138 - 커뮤니티 DEV GCP 배포 적용
    - EnableDiscoveryClient

--- community-service/src/main/java/com/freediving/communityservice/CommunityServiceApplication.java
+++ community-service/src/main/java/com/freediving/communityservice/CommunityServiceApplication.java
@@ -2,9 +2,11 @@ package com.freediving.communityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CommunityServiceApplication {
 	public static void main(String[] args) {
