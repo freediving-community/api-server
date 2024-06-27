e28af82bcbbbed6b957c926a7cbad35af278f51e
sasca37 <sasca37@naver.com>
2024-06-27 18:31:48 +0900

    deploy: #184 - 멤버서비스 githubactions gcp 이관 작업 테스트

--- member-service/src/main/java/com/freediving/authservice/adapter/in/web/CreateImgController.java
+++ member-service/src/main/java/com/freediving/authservice/adapter/in/web/CreateImgController.java
@@ -43,7 +43,7 @@ import lombok.extern.slf4j.Slf4j;
 public class CreateImgController {
 
 	private final CreateImgUseCase imgUseCase;
-
+	
 	/**
 	 * @Author           : sasca37
 	 * @Date             : 2024/01/21
