34ad69353b939a7acb8495c61a4d0de8bf0216df
YounggikimA <rladudrl286@naver.com>
2024-07-11 19:57:51 +0900

    feat: 커뮤니티 개발중 로직 제거 커밋
    - 인터셉터 권한체크 구현중 개발서버 제거

--- community-service/src/main/java/com/freediving/communityservice/adapter/in/web/CheckBoardRoleInterceptor.java
+++ community-service/src/main/java/com/freediving/communityservice/adapter/in/web/CheckBoardRoleInterceptor.java
@@ -1,12 +1,6 @@
 package com.freediving.communityservice.adapter.in.web;
 
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.HandlerMapping;
-
-import com.freediving.communityservice.adapter.out.persistence.constant.BoardType;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,18 +13,22 @@ public class CheckBoardRoleInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
 		Exception {
 
-		Map<String, String> pathVariables = (Map<String, String>)request.getAttribute(
+/*		Map<String, String> pathVariables = (Map<String, String>)request.getAttribute(
 			HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
 		String boardType = pathVariables.get("boardType");
+
+		if (boardType == null)
+			return true;
 
 		BoardType type = null;
 		try {
 			type = BoardType.valueOf(boardType.toUpperCase());
+			log.debug(type.name());
 		} catch (IllegalArgumentException e) {
 			// 매핑되지 않는 boardType인 경우 처리
 			response.setStatus(HttpStatus.BAD_REQUEST.value());
 			return false;
-		}
+		}*/
 
 		return true;
 	}
