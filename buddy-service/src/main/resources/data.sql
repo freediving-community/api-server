38cbcac303887aaddf1252a6e880af911dd44152
조준희 <46863381+RedJunHee@users.noreply.github.com>
2024-09-04 10:55:31 +0900

    feat: 다이빙풀 데이터 시간 관리 추가.

--- buddy-service/src/main/resources/data.sql
+++ buddy-service/src/main/resources/data.sql
@@ -1,44 +1,72 @@
-INSERT INTO diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
-                         price_info, website_url, recommended_level, depth, contact_number, regular_closure, is_visible,
-                         display_order)
-VALUES ('DEEPSTATION', '딥스테이션', '경기 용인시 처인구 포곡읍 성산로 523 딥스테이션', '국내 최대 수심 36m, 바오밥나무와 동굴에서 펀다이빙/사진 찍기도 좋은 다이빙풀',
-        '경기 용인시 처인구 포곡읍 성산로 523', '08:00 - 23:00 (3시간 간격)', '평일: 44,000원\r\n휴일: 66,000원', 'https://deepstation.kr/',
-        '2', '36m', NULL, NULL, false, 0),
-       ('PARADIVE', '파라다이브', '경기 시흥시 거북섬중앙로1 보니타가, 파라다이브(1동 3층)', '시흥에 위치한 최대 수심 35미터의 신생 다이빙풀, 3레벨 버디끼리 24m 입수 가능한 곳',
-        '경기 시흥시 거북섬중앙로1 보니타가', '08:00 - 23:00 (3시간 또는 6시간 간격)',
-        '일반 이용: 4.5만원 (평일)\r\n          7.9만원 (휴일)\r\n휴일 이용: 6.7만원', 'https://www.paradive.co.kr/', '2', '35m', NULL,
-        NULL, false, 0),
-       ('K26', 'K-26', '경기 가평 청평면 고성리 59-1', '서울 동부에서 접근이 좋고, 가성비가 좋은 가평 26m 수심의 다이빙풀', '경기 가평 청평면 고성리 59-1',
-        '09:00 - 22:00 (3시간 간격)', '평일: 33,000원\r\n휴일: 55,000원', 'https://k-26.com/', '2', '26m', NULL, NULL, false, 0),
-       ('THEME_SCUBA_POOL', '테마 잠수풀(TSN)', '경기 오산시 수청동 607-1', '2레벨 버디와 11m 수심을 즐길 수 있고, 창밖 기찻길 인생샷 찍기 좋은곳',
-        '경기 오산시 수청동 607-1', '09:00 - 23:00 (4시간 자유 이용)', '평일: 33,000원\r\n휴일: 50,000원', 'https://www.t-sports.kr/', '2',
-        '11m', NULL, NULL, false, 0),
-       ('OLYMPIC_DIVING_POOL', '올림픽수영장 다이빙풀', '서울시 송파구 올림픽로 424 올림픽공원 내 올림픽수영장', '서울에서 접근성이 좋고, DYN 연습하기 좋은 풀장',
-        '서울시 송파구 올림픽로 424', '토요일: 12:00 - 17:00\r\n일요일: 10:00 - 13:00\r\n       14:00 - 17:00',
-        '평일: 15,000원\r\n휴일: 20,000원', 'https://4240735.modoo.at/?link=345es9an', '1', '5m', NULL, '매월 2째 주 일요일', false,
-        0),
-       ('SONGDO_SPORTS_PARK_POOL', '송도 스포츠파크 잠수풀', '인천광역시 연수구 인천신항대로 892번길 40',
-        '예쁜 수중 벽화와 함께 5,000원에 즐길 수 있는 초가성비 송도시 운영 풀장', '인천광역시 연수구 인천신항대로 892번길 40',
-        '화요일 - 토요일: 18:00 - 21:00\r\n일요일: 09:00 - 12:00 / 13:00 - 16:00', '일일 이용료: 5,000원',
-        'https://ssp.eco-i.or.kr/reservation/dive/diving.asp?mNo=MC030000000', '1', '5m', NULL, '매주 월요일', false, 0),
-       ('SEONGNAM_AQUALINE_POOL', '성남 아쿠아라인 다목적풀', '경기도 성남시 중원구 제일로 60', '수도권 접근성이 좋고, 바닥에 설치된 거울로 자세 교정이나 강습에 좋은 풀장',
-        '경기도 성남시 중원구 제일로 60', '09:00 - 22:00', '평일: 15,000원\r\n휴일: 20,000원', 'https://aqualine.modoo.at/', '1', '5m',
-        NULL, '매월2,4주 일요일', false, 0),
-       ('WORLDCUP_SCUBA_POOL', '월드컵 스킨스쿠버 다이빙풀', '경기도 수원시 월드컵로 310 스포츠아일랜드',
-        '33m x 25m의 넓은 풀장에 드는 햇볕, 바닥에 설치된 훌라후프링에서 돌고래 놀이를 즐길 수 있는 펀다이빙 강추 풀장', '경기도 수원시 월드컵로 310',
-        '11월 - 2월: 09:00 - 21:00\r\n3월 - 10월: 09:00 - 22:00', '평일/휴일: 15,000원', 'http://m.suwonpool.com/page/page3',
-        '2', '5m', NULL, '매월2주 수요일', false, 0),
-       ('DAEBU_WELFARE_POOL', '대부동 복지체육센터', '경기도 안산시 단원구 영전로 126', '시에서 운영하는 안산시 최고 가성비 풀장', '경기도 안산시 단원구 영전로 126',
-        '1부: 09:00 - 12:00\r\n2부: 14:00 - 17:00\r\n3부: 17:30 - 20:30', '평일/휴일: 7,500원',
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('DEEPSTATION', '딥스테이션', '경기 용인시 처인구 포곡읍 성산로 523 딥스테이션', '딥스테이션은 예약제로 운영되며, 2레벨 이상 이용 가능. 평일에는 다양한 입장권이 제공됩니다.',
+        '경기 용인시 처인구 포곡읍 성산로 523', '08:00 - 23:00 (3시간 간격)', e'평일: 44,000원
+휴일: 66,000원', 'https://deepstation.kr/', '2', '36m', null, null, true, 0, '2024-04-23 02:52:36.773593',
+        '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('PARADIVE', '파라다이브', '경기 시흥시 거북섬중앙로1 보니타가, 파라다이브(1동 3층)', '파라다이브는 최대 수심 35미터로, 다이빙 연습에 적합한 환경입니다.',
+        '경기 시흥시 거북섬중앙로1 보니타가', '08:00 - 23:00 (3시간 또는 6시간 간격)', e'일반 이용: 4.5만원 (평일)
+          7.9만원 (휴일)
+휴일 이용: 6.7만원', 'https://www.paradive.co.kr/', '2', '35m', null, null, true, 1, '2024-04-23 02:52:36.773593',
+        '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('K26', 'K-26', '경기 가평 청평면 고성리 59-1', 'K-26은 실전 다이빙 연습에 최적화된 수영장으로, 예약제로 운영됩니다.', '경기 가평 청평면 고성리 59-1',
+        '09:00 - 22:00 (3시간 간격)', e'평일: 33,000원
+휴일: 55,000원', 'https://k-26.com/', '2', '26m', null, null, true, 2, '2024-04-23 02:52:36.773593',
+        '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('THEME_SCUBA_POOL', '테마 잠수풀(TSN)', '경기 오산시 수청동 607-1',
+        '테마 잠수풀은 2레벨 이상 다이버를 위해 평일 종일권을 제공하며, 다이빙의 꿈을 실현할 수 있는 곳입니다.', '경기 오산시 수청동 607-1', '09:00 - 23:00 (4시간 자유 이용)', e'평일: 33,000원
+휴일: 50,000원', 'https://www.t-sports.kr/', '2', '11m', null, null, true, 3, '2024-04-23 02:52:36.773593',
+        '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('OLYMPIC_DIVING_POOL', '올림픽수영장 다이빙풀', '서울시 송파구 올림픽로 424 올림픽공원 내 올림픽수영장', '올림픽수영장 다이빙풀은 매월 둘째 주 일요일에 휴무입니다.',
+        '서울시 송파구 올림픽로 424', e'토요일: 12:00 - 17:00
+일요일: 10:00 - 13:00
+       14:00 - 17:00', e'평일: 15,000원
+휴일: 20,000원', 'https://4240735.modoo.at/?link=345es9an', '2', '5m', null, '매월2째주일요일', true, 4,
+        '2024-04-23 02:52:36.773593', '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('SONGDO_SPORTS_PARK_POOL', '송도 스포츠파크 잠수풀', '인천광역시 연수구 인천신항대로 892번길 40',
+        '송도 스포츠파크 잠수풀은 송도시에서 운영되며, 예약제로 운영됩니다. 매주 월요일 휴무.', '인천광역시 연수구 인천신항대로 892번길 40', e'화요일 - 토요일: 18:00 - 21:00
+일요일: 09:00 - 12:00 / 13:00 - 16:00', '일일 이용료: 5,000원',
+        'https://ssp.eco-i.or.kr/reservation/dive/diving.asp?mNo=MC030000000', '2', '5m', null, '매주 월요일', true, 5,
+        '2024-04-23 02:52:36.773593', '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('SEONGNAM_AQUALINE_POOL', '성남 아쿠아라인 다목적풀', '경기도 성남시 중원구 제일로 60', '성남 아쿠아라인 다목적풀은 매월 둘째, 넷째 주 일요일에 휴무입니다.',
+        '경기도 성남시 중원구 제일로 60', '09:00 - 22:00', e'평일: 15,000원
+휴일: 20,000원', 'https://aqualine.modoo.at/', '2', '5m', null, '매월2,4주 일요일', true, 6, '2024-04-23 02:52:36.773593',
+        '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('WORLDCUP_SCUBA_POOL', '월드컵 스킨스쿠버 다이빙풀', '경기도 수원시 월드컵로 310 스포츠아일랜드', '월드컵 스킨스쿠버 다이빙풀은 매월 둘째 주 수요일 휴무입니다.',
+        '경기도 수원시 월드컵로 310', e'11월 - 2월: 09:00 - 21:00
+3월 - 10월: 09:00 - 22:00', '평일/휴일: 15,000원', 'http://m.suwonpool.com/page/page3', '2', '5m', null, '매월2주 수요일', true, 7,
+        '2024-04-23 02:52:36.773593', '2024-04-23 02:52:36.773593');
+INSERT INTO public.diving_pool (diving_pool_id, diving_pool_name, address, description, simple_address, operating_hours,
+                                price_info, website_url, recommended_level, depth, contact_number, regular_closure,
+                                is_visible, display_order, updated_date, created_date)
+VALUES ('DAEBU_WELFARE_POOL', '대부동 복지체육센터', '경기도 안산시 단원구 영전로 126',
+        '대부동 복지체육센터는 안산시에서 운영되며, 예약제로 운영됩니다. 매주 월요일 및 공휴일에 휴무.', '경기도 안산시 단원구 영전로 126', e'1부: 09:00 - 12:00
+2부: 14:00 - 17:00
+3부: 17:30 - 20:30', '평일/휴일: 7,500원',
         'https://www.ansanuc.net/homenew/10502/20005/bbsDetail.do?currentPageNo=1&searchCondition=&searchKeyword=&searchKey=&searchKey1=&searchKey3=&bbsIdx=11882',
-        '2', '5.3m', '032-880-9768', '매주 월요일/공휴일', false, 0),
-       ('CINEBLUE', '시네블루', '경기 파주시 탄현면 새오리로 427번길 120', '파주에 위치해 서울에서 가까운 6.3m의 수중 촬영장 겸 다이빙풀장',
-        '경기 파주시 탄현면 새오리로 427번길 120', '1부: 09:00 - 12:00\r\n2부: 13:30 - 17:00', '평일: 35,000원\r\n휴일: 40,000원',
-        'http://www.cineblue.co.kr', '1', '6.3m', '010-3386-1709', '전화/문자 문의 필수', false, 0),
-       ('INADIV', '인어다이브', '경기도 용인시 처인구 포곡읍 둔전로47번길 21', '3m 깊이의 ', '경기도 용인시 처인구 포곡읍 둔전로47번길 21',
-        '평일: 09:30 - 21:30까지 3시간 단위\r\n주말: 09:00 - 21:00까지 3시간 단위', '20,000원', 'https://inadiv.modoo.at/', '1', '3m',
-        '010-8902-5717', '', false, 0);
-
+        '2', '5.3m', '032-880-9768', '매주 월요일/공휴일', true, 8, '2024-04-23 02:52:36.773593', '2024-04-23 02:52:36.773593');
 
 
 INSERT INTO public.buddy_event_concept (concept_id, concept_name, concept_desc, enabled, display_order, created_date,
